d61fce9e6917e8903cb027e14dd12229abf97dbe
Ari Porad <ari@ariporad.com>
2016-02-15 11:46:16 -0700

    refactor: split `parseArgs` into it's own file

---
+++ bin/cli.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+process.exit(require('../cli')(process.argv.slice(2)));
--- cli.js
+++ cli.js
@@ -1,49 +1,59 @@
 #!/usr/bin/env node
 var shell = require('shelljs');
+var parseArgs = require('./parseArgs');
 
-if (process.argv.length < 3) {
-  console.error('Error: Missing ShellJS command name');
-  process.exit(1);
-}
+var EXIT_CODES = {
+  SHX_ERROR: 27, // https://xkcd.com/221/
+  CMD_FAILED: 1, // TODO: Once shelljs/shelljs#269 lands, use `error()`
+  SUCCESS: 0,
+};
 
-// ['foo', 'bar;baz;quz', 'qux'] => [['foo', 'bar'], ['baz'], ['quz'], ['qux']]
-function parseArgs(args) {
-  var cmds = [[]]; // An array of commands to run, each command is an array of [cmd, ...args]
-  for (var i = 0; i < args.length; ++i) {
-    var parts = args[i].split(';'); // You're allowed to separate commands with semicolons.
-    cmds[0].push(parts.shift().trim()); // Add the first thing in the array to the current command
-    if (parts.length !== 0) { // If there was >0 semicolons, then...
-      cmds.unshift([]); // Start a new command
-      args[i] = parts.join(';'); // Try it again, without the part we just handled.
-      --i;
+// This function takes the raw result of a shelljs command and figures out how to print it.
+// Invoke this *REGARDLESS* of what the command returns, it will figure it out.
+function printCmdRet(ret) {
+  // This is way to complicated. It should get much easier once shelljs/shelljs#356 is fixed.
+  if (ret) {
+    if (typeof ret.stdout !== 'undefined' && typeof ret.stderr !== 'undefined') {
+      if (ret.stdout) console.log(ret.stdout);
+      if (ret.stderr) console.error(ret.stderr);
+    } else if (ret.output) {
+      console.log(ret.output);
+    } else if (Array.isArray(ret)) {
+      console.log(ret.join('\n'));
+    } else {
+      console.log(ret);
     }
   }
-  // We store the commands in reverse order so that we can get the current one with cmds[0],
-  // so reverse them before returning it.
-  return cmds.reverse();
 }
-
-var cmds = parseArgs(process.argv.slice(2));
-
-var failed = false; // If something fails, we want to do everything else, but still exit 1
-
-for (var i = 0; i < cmds.length; ++i) {
-  if (!cmds[i]) continue; // If you did foo;bar;baz;
-  var cmd = shell[cmds[i][0]];
-  if (!cmd || typeof cmd !== 'function') throw new Error('Invalid ShellJS command: ' + cmd);
-  var ret = cmd.apply(shell, cmds[i].slice(1));
-  if (ret && typeof ret.stdout !== 'undefined' && typeof ret.stderr !== 'undefined') {
-    if (ret.stdout) console.log(ret.stdout);
-    if (ret.stderr) console.error(ret.stderr);
-  } else if (ret && ret.output) {
-    console.log(ret.output);
-  } else if (ret && Array.isArray(ret)) {
-    console.log(ret.join('\n'));
-  } else if (ret) {
-    console.log(ret);
+function shx(argv) {
+  if (argv.length < 3) {
+    console.error('Error: Missing ShellJS command name');
+    return EXIT_CODES.SHX_ERROR;
   }
 
-  if (shell.error() || (ret && ret.code && ret.code !== 0)) failed = true;
+  var cmds = parseArgs(argv);
+
+  // If something fails, we want to execute all the commands, but still error at the end.
+  var failed = false;
+
+  for (var i = 0; i < cmds.length; ++i) {
+    if (!cmds[i]) continue; // If you did foo;bar;baz;
+    var cmd = shell[cmds[i][0]];
+    if (!cmd || typeof cmd !== 'function') {
+      console.error('Error: Invalid ShellJS command: ' + cmd);
+      return EXIT_CODES.SHX_ERROR;
+    }
+    var ret = cmd.apply(shell, cmds[i].slice(1));
+    printCmdRet(ret);
+    if (shell.error() || (ret && ret.code && ret.code !== 0)) failed = true;
+  }
+
+  if (failed) {
+    return EXIT_CODES.CMD_FAILED;
+  } else {
+    return EXIT_CODES.SUCCESS;
+  }
 }
 
-if (failed) process.exit(1);
+module.exports = shx;
+module.exports._printCmdRet = printCmdRet;
