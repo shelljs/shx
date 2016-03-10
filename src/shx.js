72c00342caeee7aa4197f4f27285fac586270cf8
levithomason <me@levithomason.com>
2016-03-09 18:48:44 -0800

    refactor(shx): update shx to es6

--- src/shx.js
+++ src/shx.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
-var shell = require('shelljs');
-var parseArgs = require('./parseArgs');
-var printCmdRet = require('./printCmdRet');
+import shell from 'shelljs';
+import printCmdRet from './printCmdRet';
 
-var EXIT_CODES = {
+const EXIT_CODES = {
   SHX_ERROR: 27, // https://xkcd.com/221/
   CMD_FAILED: 1, // TODO: Once shelljs/shelljs#269 lands, use `error()`
   SUCCESS: 0,
 };
 
-var CMD_BLACKLIST = [
+const CMD_BLACKLIST = [
   'cd',
   'pushd',
   'popd',
@@ -19,36 +18,35 @@ var CMD_BLACKLIST = [
   'exec',
 ];
 
-function shx(argv) {
-  if (argv.length < 3) {
+const shx = (argv) => {
+  const [fnName, ...args] = argv.slice(2);
+  if (!fnName) {
     console.error('Error: Missing ShellJS command name');
     return EXIT_CODES.SHX_ERROR;
   }
 
-  var cmds = parseArgs(argv.slice(2));
-
-  // If something fails, we want to execute all the commands, but still error at the end.
-  var failed = false;
-
-  for (var i = 0; i < cmds.length; ++i) {
-    if (!cmds[i]) continue; // If you did foo;bar;baz;
-    var cmd = shell[cmds[i][0]];
-    if (!cmd || typeof cmd !== 'function') {
-      console.error('Error: Invalid ShellJS command: ' + cmd);
-      return EXIT_CODES.SHX_ERROR;
-    } else if (CMD_BLACKLIST.indexOf(cmd) > -1) {
-      console.error('Warning: shx ' + cmd + ' is not supported');
-      return EXIT_CODES.SHX_ERROR;
-    }
-
-    var ret = cmd.apply(shell, cmds[i].slice(1));
-    if (cmd.name !== '_echo') {
-      printCmdRet(ret);
-    }
-    if (shell.error() || (ret && ret.code && ret.code !== 0)) failed = true;
+  // validate command
+  if (typeof shell[fnName] !== 'function') {
+    console.error(`Error: Invalid ShellJS command: ${fnName}.`);
+    return EXIT_CODES.SHX_ERROR;
+  } else if (CMD_BLACKLIST.includes(fnName)) {
+    console.error(`Warning: shx ${fnName} is not supported`);
+    return EXIT_CODES.SHX_ERROR;
   }
 
-  return failed ? EXIT_CODES.CMD_FAILED : EXIT_CODES.SUCCESS;
-}
+  const ret = shell[fnName](...args);
+  const code = ret.hasOwnProperty('code') && ret.code;
 
-module.exports = shx;
+  // echo already prints
+  if (fnName !== 'echo') printCmdRet(ret);
+
+  if (typeof code === 'number') {
+    return code;
+  } else if (shell.error()) {
+    return EXIT_CODES.CMD_FAILED;
+  }
+
+  return EXIT_CODES.SUCCESS;
+};
+
+export default shx;
