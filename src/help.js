c476547d608b00becee7cd1584e944c2d134c034
Ari Porad <ari@ariporad.com>
2016-05-05 17:08:11 -0700

    refactor(help): use template strings

--- src/help.js
+++ src/help.js
@@ -5,23 +5,25 @@ const badCommands = ['ShellString', 'cd', 'pushd', 'popd', 'dirs'];
 const commandList = Object.keys(shell)
   .filter(cmd => typeof shell[cmd] === 'function' && badCommands.indexOf(cmd) === -1);
 
-const help = [
-  'shx: A wrapper for shelljs UNIX commands.',
-  '',
-  'Example:',
-  '',
-  '    $ shx ls .',
-  '    foo.txt',
-  '    bar.txt',
-  '    baz.js',
-  '    $ shx rm -rf *.txt',
-  '    $ shx ls .',
-  '    baz.js',
-  '',
-  'Commands:',
-  '',
-  ...(commandList.map(cmd => '    - ' + cmd)),
-  '',
-].join('\n');
+const help = `
+shx: A wrapper for shelljs UNIX commands.
+
+Usage: shx <command> [options]
+
+Example:
+
+    $ shx ls .
+    foo.txt
+    bar.txt
+    baz.js
+    $ shx rm -rf *.txt
+    $ shx ls .
+    baz.js
+
+Commands:
+
+${commandList.map(cmd => '    - ' + cmd).join('\n')},
+
+`;
 
 export default () => help;
