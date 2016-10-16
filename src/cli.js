b08a5d799a131a4cc61efbfe9517f4099ccd1289
Nate Fischer <ntfschr@google.com>
2016-10-16 00:17:45 -0700

    fix: don't exit until all output is flushed
    Shx previously suffered from a race condition where it could exit the process
    before stdout is fully written (especially so for lots of output). Now process
    termination is deferred until all streams end.
    
    Fixes #85

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
 import { shx } from './shx';
 
-process.exit(shx(process.argv));
+const code = shx(process.argv);
+
+// Make sure output is flushed before exiting the process. Please see:
+//  - https://github.com/shelljs/shx/issues/85
+//  - https://github.com/mochajs/mocha/issues/333
+let streamCount = 0;
+const streams = [process.stdout, process.stderr];
+streams.forEach(stream => {
+  streamCount++; // count each stream
+  stream.write('', () => {
+    streamCount--; // mark each stream as finished
+    if (streamCount === 0) process.exit(code);
+  });
+});
