f743c355cb729f140aaa2b9bc14376153cd8697e
Nate Fischer <ntfschr@gmail.com>
2016-10-30 15:24:01 -0700

    feat: pass stdin along to ShellJS commands (#89)
    Read all of stdin and pass it to the underlying ShellJS command. This is also
    adds support for mocking stdin during tests, and refactors the mock utilities.
    
    Fixes #80

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,33 @@
 #!/usr/bin/env node
 import { shx } from './shx';
 
-const code = shx(process.argv);
+// `input` is null if we're running from a TTY, or a string of all stdin if
+// running from the right-hand side of a pipe
+const run = (input) => {
+  // Pass stdin to shx as the 'this' parameter
+  const code = shx.call(input, process.argv);
 
-// Make sure output is flushed before exiting the process. Please see:
-//  - https://github.com/shelljs/shx/issues/85
-//  - https://github.com/mochajs/mocha/issues/333
-let streamCount = 0;
-const streams = [process.stdout, process.stderr];
-streams.forEach(stream => {
-  streamCount++; // count each stream
-  stream.write('', () => {
-    streamCount--; // mark each stream as finished
-    if (streamCount === 0) process.exit(code);
+  // Make sure output is flushed before exiting the process. Please see:
+  //  - https://github.com/shelljs/shx/issues/85
+  //  - https://github.com/mochajs/mocha/issues/333
+  let streamCount = 0;
+  const streams = [process.stdout, process.stderr];
+  streams.forEach(stream => {
+    streamCount++; // count each stream
+    stream.write('', () => {
+      streamCount--; // mark each stream as finished
+      if (streamCount === 0) process.exit(code);
+    });
   });
-});
+};
+
+// ShellJS doesn't support input streams, so we have to collect all input first
+if (process.stdin.isTTY) {
+  // There's no stdin, so we can immediately invoke the ShellJS function
+  run(null);
+} else {
+  // Read all stdin first, and then pass that onto ShellJS
+  const chunks = [];
+  process.stdin.on('data', data => chunks.push(data));
+  process.stdin.on('end', () => run(chunks.join('')));
+}
