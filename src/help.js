4c29f09724b4d9d86d9c89974eb5f379f84fc878
Nate Fischer <ntfschr@google.com>
2018-07-04 15:55:47 -0700

    feat: help command provides info on options
    This adds an 'shx-options' section the help output, covering options
    such as --fatal, --silent, etc.
    
    This also reformats existing help text slightly.
    
    Fixes #101

--- src/help.js
+++ src/help.js
@@ -1,27 +1,42 @@
 import shell from 'shelljs';
-import { CMD_BLACKLIST } from './config';
+import { CMD_BLACKLIST, OPTION_BLACKLIST } from './config';
+
+// Global options defined directly in shx.
+const locallyDefinedOptions = ['version'];
+
+const shxOptions = Object.keys(shell.config)
+  .filter(key => typeof shell.config[key] !== 'function')
+  .filter(key => OPTION_BLACKLIST.indexOf(key) === -1)
+  .concat(locallyDefinedOptions)
+  .map(key => `    * --${key}`);
 
 export default () => {
+  // Note: compute this at runtime so that we have all plugins loaded.
   const commandList = Object.keys(shell)
-    .filter(cmd => typeof shell[cmd] === 'function' && CMD_BLACKLIST.indexOf(cmd) === -1);
+    .filter(cmd => typeof shell[cmd] === 'function')
+    .filter(cmd => CMD_BLACKLIST.indexOf(cmd) === -1)
+    .map(cmd => `    * ${cmd}`);
 
   return `
 shx: A wrapper for shelljs UNIX commands.
 
-Usage: shx <command> [options]
+Usage: shx [shx-options] <command> [cmd-options] [cmd-args]
 
 Example:
 
     $ shx ls .
     foo.txt
-    bar.txt
     baz.js
-    $ shx rm -rf *.txt
-    $ shx ls .
+    $ shx rm -rf *.txt && shx ls .
     baz.js
 
 Commands:
 
-${commandList.map(cmd => `    - ${cmd}`).join('\n')}
+${commandList.join('\n')}
+
+Shx Options (please see https://github.com/shelljs/shelljs for details on each
+option):
+
+${shxOptions.join('\n')}
 `;
 };
