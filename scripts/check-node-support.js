635e287c4948edfbef473080b103e84f01e110fe
Nate Fischer <ntfschr@google.com>
2022-01-09 17:36:48 -0800

    chore: add back node v6 support
    No change to logic. This adds back node v6 support to partially revert
    https://github.com/shelljs/shx/pull/186. I'm hoping that v6 support is
    easier now that we rely on GitHub actions for codecov. The motivation
    for this change is so that we can make 0.3.x patch releases if
    necessary.

--- scripts/check-node-support.js
+++ scripts/check-node-support.js
@@ -8,7 +8,7 @@ var yaml = require('js-yaml');
 var shell = require('shelljs');
 
 // This is the authoritative list of supported node versions.
-var MIN_NODE_VERSION = 8;
+var MIN_NODE_VERSION = 6;
 var MAX_NODE_VERSION = 14;
 
 function checkReadme(minNodeVersion) {
